1eb597aaddf5904de611b08e2253723ade92401f
Matteo04x <59709050+Matteo04x@users.noreply.github.com>
2024-05-18 18:16:40 +0200

    Delete Master directory

--- Master/secrets.h
+++
@@ -1,8 +0,0 @@
-#define WIFI_SSID "Eolo"
-#define WIFI_PASSWORD "Bi2mKJC4e"
-
-#define MQTT_BROKER_IP "192.168.68.109"
-#define MQTT_BROKER_PORT 1883
-#define MQTT_BROKER_USERNAME "Mandorlini-Spiga"
-#define MQTT_BROKER_PASSWORD "password"
-#define MQTT_CLIENT_ID "Arduino"
